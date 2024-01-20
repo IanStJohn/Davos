93ab303ad6b673bdc3d0ebf883eb2329a6f6b59c
Ian St. John <iangstjohn@gmail.com>
2024-01-20 15:39:38 -0500

    Add dependency/constructor injection

--- src/main/java/place/ian/davos/BaseRestController.java
+++ src/main/java/place/ian/davos/BaseRestController.java
@@ -1,8 +1,11 @@
 package place.ian.davos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class BaseRestController {
@@ -24,4 +27,17 @@ public class BaseRestController {
     public String fortune() {
         return "Today is your lucky day.";
     }
+
+    private Game myGame;
+
+    @Autowired
+    public BaseRestController(Game theGame) {
+        myGame = theGame;
+    }
+
+    @GetMapping("/games/psx")
+    public String getGame() {
+        return myGame.getConsole();
+    }
+    
 }
